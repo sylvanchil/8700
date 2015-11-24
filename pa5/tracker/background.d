a334df7f4adc61c05dd75743b3b17a09f0f124d6
sylvanchil <zoomchil@gmail.com>
2015-11-23 21:04:19 -0500

    cleaned

---
+++ pa5/tracker/background.d
@@ -0,0 +1,2 @@
+background.o background.d : background.cpp background.h ioManager.h gamedata.h \
+ vector2f.h parseXML.h
---
+++ pa5/tracker/main.d
@@ -0,0 +1,4 @@
+main.o main.d : main.cpp manager.h ioManager.h gamedata.h vector2f.h parseXML.h \
+ 3ddrawable.h geo.h clock.h hudobj3ddrawable.h obj3ddrawable.h \
+ background.h ground3ddrawable.h viewpoint.h blockobj3ddrawable.h \
+ paperplaneobj3ddrawable.h
