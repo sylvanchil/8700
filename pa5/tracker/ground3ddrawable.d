d434b4267f0db7c96ea2033e2324219473bcae16
sylvanchil <zoomchil@gmail.com>
2015-11-23 21:01:34 -0500

    cleaned

--- pa5/tracker/ground3ddrawable.d
+++
@@ -1,3 +0,0 @@
-ground3ddrawable.o ground3ddrawable.d : ground3ddrawable.cpp ground3ddrawable.h \
- obj3ddrawable.h 3ddrawable.h geo.h ioManager.h gamedata.h vector2f.h \
- parseXML.h viewpoint.h aaline.h
